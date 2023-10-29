dc61a09ad69860433d0ab6397da7cd4a219cd95d
Matthew Morikan <matthew@MatthewikansMBP.lan>
2023-10-29 02:05:27 -0700

    Fixed quiz display

--- src/QuizList.jsx
+++ src/QuizList.jsx
@@ -1,16 +1,13 @@
-// QuizList.js
 import React from "react";
 import { Link } from "react-router-dom";
 
-function QuizList({ data }) {
+function QuizList() {
   return (
     <div>
       <h2>Quiz Questions</h2>
-      {data.map((question, index) => (
-        <div key={index}>
-          <Link to={`/question/${index}`}>Question {index + 1}</Link>
-        </div>
-      ))}
+      <div>
+        <Link to="/question/0">Take Quiz here</Link>
+      </div>
     </div>
   );
 }
